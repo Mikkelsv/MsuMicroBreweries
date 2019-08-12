4ecee6a51a1480e51332842657208cd5caf2dab4
Mikkel Svagård <mikkelsv@stud.ntnu.no>
2019-08-12 14:48:35 +0200

    Add beer type enums

---
+++ MicroBreweries/MicroBreweries/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroBreweries
+{
+    public enum BeerType
+    {
+        Pilsner, IPA, Pale_Ale, Wheat, Stout, Brown_Ale
+    }
+}
